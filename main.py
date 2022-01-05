70538c46a9ac98195f8161049a89505b685c2b43
Luis <hypnoticsiege@gmail.com>
2022-01-04 19:30:20 -0500

    Comments & Types

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+# This is a simple program for me to learn Python
+# Made by Luis Quezada (HypnoticSiege)
+"""
+Multi-Line Comment
+"""
 x = "Hello World!"
 y = "I'm Luis."
 
 print(x + " " + y)
+print(type(x))
